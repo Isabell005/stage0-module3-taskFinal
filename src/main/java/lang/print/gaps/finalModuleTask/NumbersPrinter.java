17694d27a75ec7dc41006ce0725969b38abcc4e8
Isabella Karibian <isabellakaribian@gmail.com>
2023-07-11 14:51:40 +0400

    stage0Module3TaskeFinal

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -2,13 +2,9 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
     public void printIsPositive(int number) {
-
-        number = 10 / 2;
-        int res = number;
-        boolean a =number==5;
-        boolean b = number!=5;
-        System.out.println(b||a);
-
+        number = 10;
+        boolean a = 5 / 2 != 2.5 || 5 / 2 <= 0 || 5 / 2 == 2.5;
+        System.out.println(a);
 
     }
 }
