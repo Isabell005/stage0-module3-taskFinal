5298b3ed9b42cfa7510904cb6457fc7fe6093723
Isabella Karibian <isabellakaribian@gmail.com>
2023-07-17 14:31:04 +0400

    stage0Module3TaskeFinal

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -1,11 +1,15 @@
 package lang.print.gaps.finalModuleTask;
 
+import java.util.Scanner;
+
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
-        number = 1111;
-        int res = 1 + 1 + 1 + 1;
-        System.out.println(res);
-
-
+        Scanner sc= new Scanner(System.in);
+        int sum=0;
+       while (number!=0){
+           sum= sum+number%10;
+           number=number/10;
+       }
+        System.out.println(sum);
     }
 }
