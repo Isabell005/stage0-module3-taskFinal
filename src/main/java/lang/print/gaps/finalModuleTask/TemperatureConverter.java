b18edd928c45ae8a3c6b8fd319463bad04409ffc
Isabella Karibian <isabellakaribian@gmail.com>
2023-09-06 12:01:33 +0400

    stage0Module3TaskeFinal

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -1,14 +1,13 @@
 package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
+    public static void main(String[] args) {
+        TemperatureConverter converter = new TemperatureConverter();
+        converter.toFahrenheit(30);
+    }
+
     public void toFahrenheit(int temperatureCelsius) {
-
-         float fahrenheit = ((temperatureCelsius * 9) / 5) + 32;
-
-
-        System.out.println(fahrenheit);
-
-
-
+        double temperatureFahrenheit = (temperatureCelsius * 9.0/5.0) + 32;
+        System.out.println(temperatureCelsius + "°C is equal to " + temperatureFahrenheit + "°F");
     }
 }
